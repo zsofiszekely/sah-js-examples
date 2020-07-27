e83892f52823a4a31e829b5d5159bd9a412acfe2
zsofiszekely <68711416+zsofiszekely@users.noreply.github.com>
2020-07-27 11:46:16 +0200

    First

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log("JavaScript is working!");
+
+let colors = ["orange", "blue", "green", "purple"]
+let post ={
+    title: "My Post",
+    description: "My First post, awesome",
+    likeCount: 12,
+    comments: ["cool!", "nice job!"]
+}
+
+console.log(colors);
+console.log(post);
